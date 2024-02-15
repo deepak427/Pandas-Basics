59e5abce516e0da0a87cd644cfaf8e40ad839a76
deepak427 <62477872+deepak427@users.noreply.github.com>
2024-02-15 16:06:10 +0530

    learned extracting data

--- main.py
+++ main.py
@@ -4,4 +4,11 @@ path = 'Data/test.csv'
 
 df = pd.read_csv(path)
 
-print(df.head())
+# print(df.head())
+# print(df.shape)
+# print(df.info())
+
+# print(df.iloc[0])
+# print(df.loc[0, 'Country'])
+# print(df.loc[:, ['Country','Month']])
+
